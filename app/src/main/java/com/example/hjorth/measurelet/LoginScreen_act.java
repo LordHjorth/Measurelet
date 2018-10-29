5c28fbd68646d48fd8ebcbbab229b9acd6ac93b6
LordHjorth <Hjorthen96@hotmail.com>
2018-10-29 10:12:13 +0100

    Classes and xml files created

---
+++ app/src/main/java/com/example/hjorth/measurelet/LoginScreen_act.java
@@ -0,0 +1,13 @@
+package com.example.hjorth.measurelet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen_act);
+    }
+}
