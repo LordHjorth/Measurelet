e2464d4ef025c5493bb05fd6570f5bd15ebc3fef
LordHjorth <Hjorthen96@hotmail.com>
2018-11-26 11:55:03 +0100

    Local database setup

--- app/src/main/java/com/measurelet/Model/Patient.java
+++ app/src/main/java/com/measurelet/Model/Patient.java
@@ -1,19 +1,30 @@
 package com.measurelet.Model;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Patient {
 
-    @PrimaryKey(autoGenerate = true)
-    public UUID id;
+    @PrimaryKey
+    public String id;
 
     @ColumnInfo(name = "PatientName")
     public String patientName;
 
+    @ColumnInfo(name = "RegDate")
+    public Date regDate;
+
+    @Ignore
+    public Patient(String patientName){
+        this.id = UUID.randomUUID().toString();
+        this.patientName = patientName;
+        this.regDate = new Date();
+    }
 }
