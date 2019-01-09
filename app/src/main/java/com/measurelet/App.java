b176c6aeecfbcc1ad9578db2ffccafceca38cbb8
mibsen <s175443@student.dtu.dk>
2019-01-09 12:23:48 +0100

    Added some signup logic

--- app/src/main/java/com/measurelet/App.java
+++ app/src/main/java/com/measurelet/App.java
@@ -1,14 +1,23 @@
 package com.measurelet;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.google.firebase.FirebaseApp;
+import com.measurelet.Database.Database_Local.LocalDatabase;
+import com.measurelet.Database.Database_Online.OnlineDatabase;
+import com.measurelet.Database.Database_Online.PatientFirebase;
 import com.measurelet.Model.Patient;
 
 public class App extends Application {
 
-    public static Boolean HasloggedIn = false;
+    public static SharedPreferences preferenceManager;
 
+    public static Boolean loggedIn = false;
+
+
+    public static PatientFirebase currentUser;
 
     // Called when the application is starting, before any other application objects have been created.
     // Overriding this method is totally optional!
@@ -17,6 +26,21 @@ public class App extends Application {
         super.onCreate();
         // Required initialization logic here!
         FirebaseApp.initializeApp(this);
+        preferenceManager = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        // Verify if a user is logged in
+
     }
+
+    public static boolean isLoggedIn(){
+
+        return false;
+    }
+
+    public static Patient getCurrentPatient(){
+        return local_database.patientDao().getFirstPatient();
+    }
+
 }
 
